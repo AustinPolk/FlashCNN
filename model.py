b1b2fdb3d5517e4cf4f6f087a2e294342aaa1d50
Austin Polk <austinpolk32@gmail.com>
2025-05-10 20:16:23 -0500

    make the model more parametrized

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from torch import nn
 import numpy as np
 
 class FlashModel(nn.Module):
-    def __init__(self, input_shape, output_shape, stations, k=5, dropout=0.2, sigmoid_output=False, convolutional_layers=2):
+    def __init__(self, input_shape, output_shape, stations, k=5, pool_k=2, dropout=0.2, convolutional_layers=2, channel_multiplier=2, variables_per_station=6):
         super(FlashModel, self).__init__()
         
         self.channels = input_shape[0]
@@ -11,29 +11,13 @@ class FlashModel(nn.Module):
         self.input_features = input_shape[2]
         self.output_features = output_shape[0]
         self.kernel_size = k
+        self.pool_kernel_size = pool_k
         self.dropout_rate = dropout
         self.stations = stations
-        self.sigmoid_output = sigmoid_output
         self.convolutional_layers = convolutional_layers
-        self.variables_per_station = 6
-        self._check_viability()
+        self.channel_multiplier = channel_multiplier
+        self.variables_per_station = variables_per_station
         self.network = self._create_network()
-
-    def _check_viability(self):
-        c, h, w = self.channels, self.sequence_length, self.input_features
-        print(c, h, w)
-        c, h, w = 4*c, h, 1
-        print(c, h, w)
-        for i in range(self.convolutional_layers):
-            c, h = 2*c, h - self.kernel_size + 1
-            print(c, h, w)
-            h = h / 2
-            print(c, h, w)
-        t = c * h * w
-        print(t)
-
-        if not t.is_integer():
-            raise Exception("Invalid CNN Config")
     
     def _create_network(self):
         network = nn.Sequential()
@@ -42,18 +26,18 @@ class FlashModel(nn.Module):
         print(c, h, w)
         
         # first convolutional layer, followed by relu and maxpooling layers
-        network.add_module('Conv1', nn.Conv2d(in_channels=self.channels, out_channels=4*self.channels, kernel_size=(1, self.input_features)))
-        c, h, w = 4*c, h, 1
+        network.add_module('Conv1', nn.Conv2d(in_channels=self.channels, out_channels=2*self.channel_multiplier*c, kernel_size=(1, self.input_features)))
+        c, h, w = 2*self.channel_multiplier*c, h, 1
         print(c, h, w)
     
         for i in range(self.convolutional_layers):
-            network.add_module(f'Conv{i + 2}', nn.Conv2d(in_channels=4*self.channels, out_channels=8*self.channels, kernel_size=(self.kernel_size, 1)))
-            c, h = 2*c, h - self.kernel_size + 1
+            network.add_module(f'Conv{i + 2}', nn.Conv2d(in_channels=c, out_channels=self.channel_multiplier*c, kernel_size=(self.kernel_size, 1)))
+            c, h = self.channel_multiplier*c, h - self.kernel_size + 1
             print(c, h, w)
             
             network.add_module(f'ReLU{i+1}', nn.ReLU())
-            network.add_module(f'MaxPool{i+1}', nn.MaxPool2d(kernel_size=(2,1), stride=2))
-            h = h // 2
+            network.add_module(f'MaxPool{i+1}', nn.MaxPool2d(kernel_size=(self.pool_kernel_size, 1), stride=self.pool_kernel_size))
+            h = h // self.pool_kernel_size
             print(c, h, w)
         
         # then flatten everything into a single dimension and apply dropout
@@ -72,12 +56,5 @@ class FlashModel(nn.Module):
         return network
     def forward(self, x):
         y = self.network(x)
-
-        # apply sigmoid function to first variable for each station, multiplied by 100 to be on the scale of everything else
-        if self.sigmoid_output:
-            for station_idx in range(self.stations):
-                target_idx = self.variables_per_station * station_idx
-                y[-1, target_idx] = 100.0 * torch.sigmoid(y[-1, target_idx])
-
         return y
                 
