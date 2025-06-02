c3dec6392c0756b7721908534409ef37ac5eb268
Austin Polk <austinpolk32@gmail.com>
2025-06-02 12:58:20 -0500

    Begin developing new simple model

---
+++ oml/model.py
@@ -0,0 +1,76 @@
+import torch
+from torch import nn
+import numpy as np
+import pandas as pd
+
+# a single FlashCNN module that models the weather at one specific "point" (station, grid cell, etc.)
+class FlashPoint(nn.Module):
+    def __init__(self, num_variables, lookback, lookahead):
+        super(FlashPoint, self).__init__()
+        
+        self.input_length = lookback
+        self.input_features = num_variables
+        self.output_features = num_variables
+        self.output_length = lookahead
+        self.convolutional, output_size = self._create_convolutional(5, 2, 8, True)
+        self.fully_connected = self._create_fully_connected(output_size, 256)
+    
+    def _create_convolutional(self, k, p, z, switch):
+        network = nn.Sequential()
+    
+        c, cc, h, w = 1, z, self.input_length, self.input_features
+        
+        if switch:
+            # compress features along the width of the input matrix using a conv kernel
+            network.add_module('Conv1', nn.Conv2d(in_channels=c, out_channels=cc, kernel_size=(1, w)))
+            c, cc, h, w = cc, 2*cc, h, 1
+
+            # perform convolutions along the remaining dimension, use ReLU and maxpooling
+            network.add_module(f'Conv2', nn.Conv2d(in_channels=c, out_channels=cc, kernel_size=(k, 1)))
+            network.add_module(f'ReLU2', nn.ReLU())
+            network.add_module(f'MaxPool2', nn.MaxPool2d(kernel_size=(p, 1), stride=p))
+            c, cc, h = cc, 2*cc, (h - k + 1)//p
+        else:
+            # optionally combine the above two kernels into a single, larger one
+            network.add_module(f'Conv2', nn.Conv2d(in_channels=c, out_channels=2*cc, kernel_size=(k, w)))
+            network.add_module(f'ReLU2', nn.ReLU())
+            network.add_module(f'MaxPool2', nn.MaxPool2d(kernel_size=(p, 1), stride=p))
+            c, cc, h, w = cc, 4*cc, (h - k + 1)//p, 1
+
+        network.add_module(f'Conv3', nn.Conv2d(in_channels=c, out_channels=cc, kernel_size=(k, 1)))
+        network.add_module(f'ReLU3', nn.ReLU())
+        network.add_module(f'MaxPool3', nn.MaxPool2d(kernel_size=(p, 1), stride=p))
+        c, cc, h = cc, 2*cc, (h - k + 1)//2
+
+        # flatten output and apply dropout
+        network.add_module('Flatten', nn.Flatten())
+        network.add_module('Dropout', nn.Dropout(0.2))
+
+        # return the network and the output size
+        return network, int(c*h)
+
+    def _create_fully_connected(self, input_size, h):
+        network = nn.Sequential()
+
+        # apply fully connected layers to output of convolutional layers
+        network.add_module('FC1', nn.Linear(input_size, h))
+        network.add_module('ReLUFC1', nn.ReLU())
+        network.add_module('FC2', nn.Linear(h, h//4))
+        network.add_module('ReLUFC2', nn.ReLU())
+        network.add_module('FC3', nn.Linear(h//4, self.output_features*self.output_length))
+        
+        # unflatten the output to stack the predictions for each day (unflatten dim=1 since it is expected to be batched in dim=0)
+        network.add_module('Unflatten', nn.Unflatten(dim=1, unflattened_size=(self.output_length, self.output_features)))
+    
+        return network
+    def forward(self, x):
+        # run the input through the convolutional layers, then through the fully connected layers
+        conv = self.convolutional(x)
+        y = self.fully_connected(conv)
+        return y
+    def copy(self):
+        m = FlashPoint(self.input_features, self.input_length, self.output_length)
+        m.load_state_dict(self.load_state_dict())
+        return m
+    def copy_from(self, other):
+        self.load_state_dict(other.load_state_dict())
---
+++ oml/train.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import torch
+from oml.model import FlashPoint
+from torch import nn, optim
+import numpy as np
+import torch.utils.data as Data
+
+def create_tensors(from_data: pd.DataFrame, variables: list):
+    date_lookup = {}
+    index = from_data['DATE']
+    for i in range(len(index)):
+        date_lookup[index[i]] = i
+
+    stations = from_data['STATION'].unique()
+    station_tensors = {}
+    for station in stations:
+        station_specific = from_data[from_data['STATION'] == station]
+        station_tensors[station] = torch.from_numpy(station_specific[variables].values)
+
+    return station_tensors, date_lookup
+
+def create_datasets(tensor: torch.Tensor, lookback: int, lookahead: int):
+    first_idx = lookback + 1
+    last_idx = tensor.size()[0] - lookahead - 1
+    
+    X = []
+    Y = []
+
+    for idx in range(first_idx, last_idx, 1):
+        b1, b2 = idx - lookback, idx - 1
+        input_features = tensor[b1:b2]
+        a1, a2 = idx, idx + lookahead - 1
+        output_features = tensor[a1:a2]
+
+        X.append(input_features)
+        Y.append(output_features)
+    
+    return torch.stack(X, dim=0), torch.stack(Y, dim=0)
+
+def train(model: FlashPoint, train: tuple, validate: tuple, learning_rate=0.001, batch_size=8, epochs=100):
+    loss_fn = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    train_X, train_Y = train
+    val_X, val_Y = validate
+
+    dataset = Data.TensorDataset(train_X, train_Y)
+    loader = Data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    model.eval()
+    with torch.no_grad():
+        Y_pred = model(val_X)
+        val_rmse = np.sqrt(loss_fn(Y_pred, val_Y))
+        print(f'Current model test RMSE: {val_rmse}')
+
+    training_losses = np.zeros(epochs)
+    validation_losses = np.zeros(epochs)
+
+    checkpoint_model = model.copy()
+
+    best_val_rmse = val_rmse
+    for epoch in range(epochs):
+        model.train()
+        for X_batch, y_batch in loader:
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+        # Validation
+        model.eval()
+        with torch.no_grad():
+            Y_pred = model(train_X)
+            train_rmse = np.sqrt(loss_fn(Y_pred, train_Y))
+            training_losses[epoch] = np.square(train_rmse)
+            Y_pred = model(val_X)
+            val_rmse = np.sqrt(loss_fn(Y_pred, val_Y))
+            validation_losses[epoch] = np.square(val_rmse)
+        
+        if (epoch + 1) % 5 == 0:
+            print("Epoch %d: train RMSE %.4f, val RMSE %.4f" % (epoch+1, train_rmse, val_rmse))
+    
+        if val_rmse < best_val_rmse:
+            best_val_rmse = val_rmse
+            print("Checkpointing at epoch %d with val RMSE %.4f" % (epoch + 1, val_rmse))
+            checkpoint_model.copy_from(model)
+
+    return checkpoint_model, training_losses, validation_losses
+
+    
+
+    
